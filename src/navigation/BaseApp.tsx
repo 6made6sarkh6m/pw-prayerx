3ac78b4b3d97ed695f704d4fb592225868e220fd
Temirlan <temirlanamanzholov@astanait.edu.kz>
2022-02-23 15:14:41 +0600

    update(BaseApp): delete StackNavigator

--- src/navigation/BaseApp.tsx
+++ src/navigation/BaseApp.tsx
@@ -4,7 +4,6 @@ import {useSelector} from 'react-redux';
 import {selectToken} from '../redux/ducks/Auth/selectors';
 import AuthNavigation from './AuthNavigation/AuthNavigation';
 import UserNavigation from './UserNavigation/UserNavigation';
-const Stack = createStackNavigator();
 
 const BaseApp = () => {
   const isToken = useSelector(selectToken);
